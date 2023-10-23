30d7a9db0ae3bc4d5c3f49f13680e05defce245c
lmpcas2023 <145418453+lmpcas2023@users.noreply.github.com>
2023-10-23 17:02:27 +1000

    updated validators for login/register forms

--- musimate/forms.py
+++ musimate/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.validators import InputRequired, Email, EqualTo, Length, DataRequired
 from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
@@ -18,21 +18,20 @@ class EventForm(FlaskForm):
     
 #User login
 class LoginForm(FlaskForm):
-    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
-    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name = StringField("User Name", validators=[DataRequired(message='Enter user name'), Length(min=3, max=64, message="Username must be between 3 and 64 characters")])
+    password = PasswordField("Password", validators=[DataRequired(message='Enter user password')])
     submit = SubmitField("Login")
 
 #User register
 class RegisterForm(FlaskForm):
-    user_name = StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    
+    user_name = StringField("User Name", validators=[DataRequired(), Length(min=4, max=32, message="Username must be between 4 and 32 characters")])
+    email_id = StringField("Email Address", validators=[DataRequired(), Email("Please enter a valid email"),Length(max=32, message="Email must be under 32 characters")])
     #linking two fields - password should be equal to data entered in confirm
-    password = PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message="Password must be at least 6 characters long"), EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password", validators=[DataRequired()])
     #submit button
     submit = SubmitField("Register")
+
 
 #User comment
 class CommentForm(FlaskForm):
